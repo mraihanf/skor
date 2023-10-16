6594929917bba8f306ecad154749f0adfb282d3d
Muhammad Raihan Fadhlullah <fadhlullah24@gmail.com>
2023-10-16 17:50:58 +0700

    first commit

---
+++ src/main/java/skor/SkorApplication.java
@@ -0,0 +1,13 @@
+package skor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skor/SkorApplicationTests.java
@@ -0,0 +1,13 @@
+package skor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
